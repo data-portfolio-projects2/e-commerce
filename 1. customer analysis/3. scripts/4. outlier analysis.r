ee73fbfea0b02332a0ed504b80e1a1571fa59f2d
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-01-22 21:56:14 +0800

    Update 4. outlier analysis.r

--- 1. customer analysis/3. scripts/4. outlier analysis.r
+++ 1. customer analysis/3. scripts/4. outlier analysis.r
@@ -1,15 +1,15 @@
 income_bins <- c(-Inf, 15000, 24999, 34999, 49999, 74999, 99999, 149999, 199999, Inf)
 
-df$Income_category <- cut(df$Income, breaks = income_bins, 
+df$Income_Category <- cut(df$Income, breaks = income_bins, 
                           labels = c("lower_outlier", "$15,000 to $24,999", "$25,000 to $34,999", 
                                      "$35,000 to $49,999", "$50,000 to $74,999", "$75,000 to $99,999", 
                                      "$100,000 to $149,999", "$150,000 to $199,999", "upper_outlier"),
                           right = FALSE)
 
-head(df$Income_category)
+head(df$Income_Category)
 
-outliers_below_min <- sum(df$Income_category == "lower_outlier")  
-outliers_above_max <- sum(df$Income_category == "upper_outlier") 
+outliers_below_min <- sum(df$Income_Category == "lower_outlier")  
+outliers_above_max <- sum(df$Income_Category == "upper_outlier") 
 
 cat("outliers below minimum:", outliers_below_min, "\n")
 cat("outliers above maximum:", outliers_above_max, "\n")
