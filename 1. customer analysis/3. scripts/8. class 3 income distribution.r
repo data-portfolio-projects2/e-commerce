950ea90b2e0c748cf43166506493aaeffa0d54ef
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-01-24 21:05:31 +0800

    Create 8. class 3 income distribution.r

---
+++ 1. customer analysis/3. scripts/8. class 3 income distribution.r
@@ -0,0 +1,11 @@
+income_subset <- subset(df, Income_Category == "$50,000 to $74,999")
+
+boxplot_income <- ggplot(income_subset, aes(y = Income)) +
+  geom_boxplot(fill = "skyblue") +
+  labs(title = "Boxplot for $50,000 to $74,999 Income", y = "Income Values") +
+  theme_minimal()
+
+histogram_income <- ggplot(income_subset, aes(x = Income)) +
+  geom_histogram(fill = "lightgreen", bins = 20, color = "black") +
+  labs(title = "Histogram for $50,000 to $74,999 Income", x = "Income Values", y = "Frequency") +
+  theme_minimal()
