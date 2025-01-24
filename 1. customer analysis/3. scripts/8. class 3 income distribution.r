6848bc96e7c3e09f6591ac4a7e594734d406c83c
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-01-24 22:06:08 +0800

    Update 8. class 3 income distribution.r

--- 1. customer analysis/3. scripts/8. class 3 income distribution.r
+++ 1. customer analysis/3. scripts/8. class 3 income distribution.r
@@ -9,3 +9,8 @@ histogram_income <- ggplot(income_subset, aes(x = Income)) +
   geom_histogram(fill = "lightgreen", bins = 20, color = "black") +
   labs(title = "Histogram for $50,000 to $74,999 Income", x = "Income Values", y = "Frequency") +
   theme_minimal()
+
+ggplot(income_subset, aes(x = seq_along(Income), y = Income)) + 
+  geom_point(color = "blue") +
+  labs(title = "Scatterplot for $50,000 to $74,999 Income", x = "Index", y = "Income") +
+  theme_minimal()
