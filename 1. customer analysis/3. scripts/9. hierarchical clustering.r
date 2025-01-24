b57173be79d90bf2e3c1224457735c7fea769511
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-01-25 00:11:01 +0800

    Update 9. hierarchical clustering.r

--- 1. customer analysis/3. scripts/9. hierarchical clustering.r
+++ 1. customer analysis/3. scripts/9. hierarchical clustering.r
@@ -7,29 +7,3 @@ hc_cosine_sample <- hclust(cosine_dist_matrix_sample, method = "ward.D2")
 plot(hc_cosine_sample, main = "Hierarchical Clustering (Sampled Data)")
 
 clusters <- cutree(hc_cosine, k = 4) 
-
-ggplot(income_subset, aes(x = Income, fill = as.factor(Cluster))) +
-  geom_histogram(binwidth = 5000, alpha = 0.7, position = "dodge") +
-  facet_wrap(~Cluster, scales = "free_y") +  
-  labs(
-    title = "Income Distribution by Cluster",
-    x = "Income",
-    y = "Count",
-    fill = "Cluster"
-  ) +
-  scale_fill_viridis_d() +  
-  theme_minimal()
-
-geom_histogram(binwidth = 1000, alpha = 0.7, position = "dodge")
-
-ggplot(income_subset, aes(x = Income, fill = as.factor(Cluster))) +
-  geom_histogram(binwidth = 5000, alpha = 0.5, position = "identity") +
-  labs(
-    title = "Overlapping Income Distribution by Cluster",
-    x = "Income",
-    y = "Count",
-    fill = "Cluster"
-  ) +
-  scale_fill_viridis_d() +
-  theme_minimal()
-
