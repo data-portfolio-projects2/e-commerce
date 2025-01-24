5fe23dfc3a6f41c81bcbeebaf34d35e3f6c562c1
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-01-25 00:01:24 +0800

    Update 9. hierarchical clustering.r

--- 1. customer analysis/3. scripts/9. hierarchical clustering.r
+++ 1. customer analysis/3. scripts/9. hierarchical clustering.r
@@ -1,12 +1,9 @@
 set.seed(123)  
 sample_indices <- sample(seq_len(nrow(scaled_income)), size = 100) 
 sampled_scaled_income <- scaled_income[sample_indices, ]
-
 cosine_dist_matrix_sample <- proxy::dist(sampled_scaled_income, method = "cosine")
 cosine_dist_matrix_sample <- as.dist(cosine_dist_matrix_sample)
-
 hc_cosine_sample <- hclust(cosine_dist_matrix_sample, method = "ward.D2")
-
 plot(hc_cosine_sample, main = "Hierarchical Clustering (Sampled Data)")
 
 clusters <- cutree(hc_cosine, k = 4) 
